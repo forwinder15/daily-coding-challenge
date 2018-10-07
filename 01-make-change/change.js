f268bbbad221c138fa357b55bc6835c4715fcb4f
john coker <jrogercoker@gmail.com>
2018-10-06 23:33:05 -0400

    Fixed working correctly. returning number of instances

--- 01-make-change/change.js
+++ 01-make-change/change.js
@@ -1,27 +1,31 @@
-
-
 let coins = [];
 function change(n){
-    coins=[];
+    coins=[0,0,0,0];
     while(n!=0){
         if (n>=25){
-            coins.push("25");
-            n = n-25;
+          const di = Math.floor(n/25);
+            coins[0] = di;
+           n = n % 25;
         }
         else if (n>=10){
-            coins.push("10");
-            n = n-10;
+          const di = Math.floor(n/10);
+            coins[1] = di;
+           n = n % 10;
         }
         else if (n>=5){
-            coins.push("5");
-            n = n -5;
+          const di = Math.floor(n/5);
+            coins[2] = di;
+           n = n % 5;
         }
         else if (n>=1){
-            coins.push("1");
-            n = n - 1;
-        }
+          const di = Math.floor(n/1);
+            coins[3] = di;
+           n = n % 1;
+      }
     }
     console.log(coins);
-}
+  }
 
-console.log(change(83));
+console.log(change(34));
+console.log(change(78));
+console.log(change(21));
